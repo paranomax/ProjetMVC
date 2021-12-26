d10a6d3e6dcd969aa815af3951a7e3c5f2aed98e
paranomax <paranomax98@gmail.com>
2021-12-26 12:36:48 +0100

    mise en place de la BD (1)

--- ProjetMVC/Models/Classe.cs
+++ ProjetMVC/Models/Classe.cs
@@ -2,10 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace ProjetMVC.Models
 {
-    public class Classe
+    public class User
     {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Address { get; set; }
+
+    }
+
+    public class Certificat
+    {
+        public int CertificatID { get; set; }
+        public int UserID { get; set; }
+        public int WeaponID { get; set; }
+        public DateTime DateBegin { get; set; }
+        public DateTime DateEnd { get; set; }
+    }
+
+    public class Weapon
+    {
+        public int WeaponID { get; set; }
+        public string WeaponModel { get; set; }
+        public string AmmoID { get; set; }
+
+    }
+
+    public class Ammo
+    {
+        public int AmmoID { get; set; }
+        public string Type { get; set; }
+        public int Caliber { get; set; }
+
+    }
+    public class Model : DbContext
+    {/*
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set;} */
     }
 }
